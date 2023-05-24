5f9384e0e4abf02493672b0cbdfdfb51358a314e
amal <amaldude@proton.me>
2023-05-24 17:17:14 +0530

    We Do A Little Trolling 💀

--- Sample.md
+++
@@ -1,3 +0,0 @@
-# Sample
-
-this is just sample file for contribution tutorial purpose
---
+++ rickgen.py
@@ -0,0 +1,13 @@
+import subprocess
+
+verse = """Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you"""
+
+verseWords = verse.split()
+
+for word in verseWords:
+    subprocess.run(f"touch {word}", shell=True)
